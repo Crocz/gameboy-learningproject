26e12266f5319763dc8e13c15be00c13f74e24e5
Richard Nordström <croczter@gmail.com>
2020-11-22 13:04:13 +0100

    Add bootrom loading. Integration test skeleton.

---
+++ GBEmu.Test/IntegrationTest.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace GBEmu.Test {
+
+    public class IntegrationTest {
+        [Test]
+        public void Test1() {
+            var testmemory = new Memory();
+            var gbCpu = new GbCpu(GbModel.DMG, testmemory);
+            for(; ; ) {
+                gbCpu.Tick();
+            }
+        }
+    }
+}
--- GBEmu/Memory.cs
+++ GBEmu/Memory.cs
@@ -1,13 +1,19 @@
-﻿namespace GBEmu {
+﻿using System;
+
+namespace GBEmu {
     public interface IMemory {
         byte ReadByte(ushort address);
         void WriteByte(ushort address, byte content);
     }
 
     public class Memory : IMemory {
+        private const string bootrompath = @"C:\Users\AdminApan\Source\Repos\gameboy-learningproject\binaries\bootrom\DMG_ROM.bin";
+
         private byte[] storage;
 
         // Memory range bounds
+        private const ushort BootRomLo = 0x0000;
+        private const ushort BootRomHi = 0x00FF;
         private const ushort Rom0Lo = 0x0000;
         private const ushort Rom0Hi = 0x3FFF;
         private const ushort RomXLo = 0x4000;
@@ -34,6 +40,7 @@
 
         public Memory() {
             storage = new byte[0xFFFF];
+            LoadBootRom(bootrompath);
         }
         public byte ReadByte(ushort address) {
             return storage[address];
@@ -41,6 +48,15 @@
 
         public void WriteByte(ushort address, byte content) {
             storage[address] = content;
+        }
+
+        private void LoadBootRom(string path) {
+            using (var fileStream = System.IO.File.OpenRead(path)) {
+                if(fileStream.Length > ( (BootRomHi + 1)- BootRomLo)) {
+                    throw new ArgumentException("BootRom exceeds maximum length.");
+                }
+                fileStream.Read(storage, BootRomLo, BootRomHi + 1);
+            }
         }
     }
 }
